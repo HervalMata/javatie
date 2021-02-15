da7f3dd1f0246b01180e6ed8dac575bf61ce72d8
HervalMata <hervalmataamparo@gmail.com>
2021-02-15 17:14:54 -0300

    Entity Categoria

---
+++ src/main/java/com/herval/javatie/domain/model/Categoria.java
@@ -0,0 +1,47 @@
+package com.herval.javatie.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome_categoria")
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return id.equals(categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
