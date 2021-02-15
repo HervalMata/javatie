1be4a7c4197d04ca1dc0766f8888525573acf0ba
HervalMata <hervalmataamparo@gmail.com>
2021-02-15 14:23:27 -0300

    Initial commit

---
+++ src/main/java/com/herval/javatie/JavatieApplication.java
@@ -0,0 +1,13 @@
+package com.herval.javatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavatieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavatieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herval/javatie/JavatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herval.javatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavatieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
